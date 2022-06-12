271c34438b2b136ee7e9553550dfab634e2c2984
Luca Di Fazio <highliuk@gmail.com>
2022-06-12 18:30:35 +0200

    🔧 Custom Google Font with Tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Quiz App</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import type { NextPage } from "next"
-import Head from "next/head"
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Quiz App</title>
-      </Head>
-
       <h1>Quiz</h1>
     </>
   )
