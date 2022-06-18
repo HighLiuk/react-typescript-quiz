b427ab6026b807e968c2ef34606d32f5bc2f716e
Luca Di Fazio <highliuk@gmail.com>
2022-06-18 19:12:48 +0200

    ✨ Add Basic Markup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
 import type { NextPage } from "next"
+import { useState } from "react"
 
 const Home: NextPage = () => {
+  const [score, setScore] = useState(0)
+
+  async function startTrivia() {}
+
+  function nextQuestion() {}
+
   return (
     <>
-      <h1>Quiz</h1>
+      <h1 className="uppercase">React Quiz</h1>
+
+      <button className="block" onClick={startTrivia}>
+        Start
+      </button>
+
+      <p>Score: {score}</p>
+
+      <p>Loading Questions...</p>
+
+      {/* Question Card */}
+
+      <button onClick={nextQuestion}>Next Question</button>
     </>
   )
 }
